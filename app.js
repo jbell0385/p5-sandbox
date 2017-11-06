b962b6cd137ad7a9911d74291d6f4d4923b69e77
Jeremy Bell <jbell0358@gmail.com>
2017-11-06 12:23:06 -0800

    adds rectangle class

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let bubbles = [];
-
+var rect1;
 
 
 function setup() {
@@ -9,6 +9,7 @@ function setup() {
     let r = random(10, 50);
     let b = new Bubble(x, y, r);
     bubbles.push(b);
+    rect1 = new Rectangle(random(width), random(height), 50, 50);
 }
 
 function draw() {
@@ -17,12 +18,15 @@ function draw() {
         bubbles[i].move();
         bubbles[i].show();
     }
+    rect1.show();
+    rect1.move();
 }
 
 function mousePressed() {
     for (let i = 0; i < bubbles.length; i++) {
         bubbles[i].clicked(mouseX, mouseY);
     }
+    rect1.clicked(mouseX,mouseY);
 }
 
 class Bubble {
@@ -33,10 +37,10 @@ class Bubble {
         this.brightness = 0;
     }
 
-    clicked(x,y) {
+    clicked(x, y) {
         let d = dist(x, y, this.x, this.y);
         if (d < this.r) {
-            this.brightness=255;
+            this.brightness = 255;
         }
     }
 
@@ -48,7 +52,33 @@ class Bubble {
     show() {
         stroke(255);
         strokeWeight(4);
-        fill(this.brightness,125);
+        fill(this.brightness, 125);
         ellipse(this.x, this.y, this.r * 2);
+    }
+}
+
+class Rectangle {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    clicked(x, y) {
+        if (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h){
+            console.log('clicked');
+        }
+    }
+
+    show() {
+        stroke(255);
+        rect(this.x, this.y, this.w, this.h);
+        noFill();
+    }
+
+    move() {
+        this.x = this.x + random(-2, 2);
+        this.y = this.y + random(-2, 2);
     }
 }
