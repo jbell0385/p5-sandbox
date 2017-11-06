c3ae10ce29573c8f6c1dc758339ae76e57c07496
Jeremy Bell <jbell0358@gmail.com>
2017-11-06 12:41:40 -0800

    add loop to create multiple circles

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let bubbles = [];
-var rect1;
+let rect1;
 
 
 function setup() {
     createCanvas(600, 400);
-    let x = random(width);
-    let y = random(height);
-    let r = random(10, 50);
-    let b = new Bubble(x, y, r);
-    bubbles.push(b);
+    for(var i=0;i<5;i++){
+        let x = random(width);
+        let y = random(height);
+        let r = random(10, 50);
+        let b = new Bubble(x, y, r);
+        bubbles.push(b);
+    }
+
     rect1 = new Rectangle(random(width), random(height), 50, 50);
 }
 
@@ -63,18 +66,20 @@ class Rectangle {
         this.y = y;
         this.w = w;
         this.h = h;
+        this.brightness = 0;
     }
 
     clicked(x, y) {
         if (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h){
             console.log('clicked');
+
         }
     }
 
     show() {
         stroke(255);
         rect(this.x, this.y, this.w, this.h);
-        noFill();
+        fill(this.brightness);
     }
 
     move() {
