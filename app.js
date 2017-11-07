5bbbedb3bfce1d29dc4120dfde9400589a232eb9
Jeremy Bell <jbell0358@gmail.com>
2017-11-06 16:06:52 -0800

    created collision detection

--- app.js
+++ app.js
@@ -1,56 +1,72 @@
-let bubbles = [];
-
-
+let b1;
+let b2;
 
 function setup() {
     createCanvas(600, 400);
-    for(let i =0; i<5;i++){
-        let x = random(width);
-        let y = random(height);
-        let r = random(10, 50);
-        let b = new Bubble(x, y, r);
-        bubbles.push(b);
-    }
+    b1 = new Bubble(250, 200);
+    b2 = new Bubble(350, 200);
 }
 
 function draw() {
     background(0);
-    for (let i = 0; i < bubbles.length; i++) {
-        bubbles[i].move();
-        bubbles[i].show();
+    b1.update();
+    b2.update();
+    b1.display();
+    b2.display();
+
+    if(b1.intersects(b2)){
+        b1.changeColor();
+        b2.changeColor();
     }
+
 }
 
-function mousePressed() {
-    for (let i = 0; i < bubbles.length; i++) {
-        bubbles[i].clicked(mouseX, mouseY);
-    }
-}
+// function mousePressed() {
+//     for (let i = bubbles.length-1; i >= 0; i--) {
+//         if (bubbles[i].contains(mouseX, mouseY)) {
+//             bubbles.splice(i, 1);
+//         }
+//     }
+// }
+//
+// function mouseMoved() {
+//     for (let i = 0; i < bubbles.length; i++) {
+//         if (bubbles[i].contains(mouseX, mouseY)) {
+//             bubbles[i].changeColor(255);
+//         } else {
+//             bubbles[i].changeColor(0);
+//         }
+//     }
+// }
 
 class Bubble {
-    constructor(x, y, r) {
-        this.x = y;
+    constructor(x, y) {
+        this.x = x;
         this.y = y;
-        this.r = r;
-        this.brightness = 0;
-    }
+        this.r = 48;
+        this.col=color(255);
 
-    clicked(x,y) {
-        let d = dist(x, y, this.x, this.y);
-        if (d < this.r) {
-            this.brightness=255;
-        }
     }
-
-    move() {
-        this.x = this.x + random(-3, 3);
-        this.y = this.y + random(-3, 3);
-    }
-
-    show() {
+    display() {
         stroke(255);
-        strokeWeight(4);
-        fill(this.brightness,125);
+        fill(this.col, 100);
         ellipse(this.x, this.y, this.r * 2);
+    }
+    update() {
+        this.x = this.x + random(-1, 1);
+        this.y = this.y + random(-1, 1);
+    }
+
+    changeColor(){
+        this.col=color(random(255),random(255),random(255));
+    }
+
+    intersects(other){
+        var d = dist(this.x, this.y, other.x, other.y);
+        if (d < this.r + other.r) {
+            return true;
+        }else{
+            return false;
+        }
     }
 }
