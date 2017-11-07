559aeef1a7cff5333fbf6fab0706251bb80ca31f
Jeremy Bell <jbell0358@gmail.com>
2017-11-07 12:37:53 -0800

    added collision detection

--- app.js
+++ app.js
@@ -1,50 +1,33 @@
-let b1;
-let b2;
+let bubbles = [];
 
 function setup() {
     createCanvas(600, 400);
-    b1 = new Bubble(250, 200);
-    b2 = new Bubble(350, 200);
+    for (var i = 0; i < 20; i++) {
+        bubbles[i] = new Bubble(random(width), random(height));
+    }
 }
 
 function draw() {
     background(0);
-    b1.update();
-    b2.update();
-    b1.display();
-    b2.display();
-
-    if(b1.intersects(b2)){
-        b1.changeColor();
-        b2.changeColor();
+    for (var i = 0; i < bubbles.length; i++) {
+        bubbles[i].update();
+        bubbles[i].display();
+        for (var j = 0; j < bubbles.length; j++) {
+            if (bubbles[i].intersects(bubbles[j]) && bubbles[j]!==bubbles[i]) {
+                bubbles[i].changeColor();
+                bubbles[j].changeColor();
+            }
+        }
     }
-
 }
 
-// function mousePressed() {
-//     for (let i = bubbles.length-1; i >= 0; i--) {
-//         if (bubbles[i].contains(mouseX, mouseY)) {
-//             bubbles.splice(i, 1);
-//         }
-//     }
-// }
-//
-// function mouseMoved() {
-//     for (let i = 0; i < bubbles.length; i++) {
-//         if (bubbles[i].contains(mouseX, mouseY)) {
-//             bubbles[i].changeColor(255);
-//         } else {
-//             bubbles[i].changeColor(0);
-//         }
-//     }
-// }
 
 class Bubble {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.r = 48;
-        this.col=color(255);
+        this.col = color(255);
 
     }
     display() {
@@ -57,15 +40,15 @@ class Bubble {
         this.y = this.y + random(-1, 1);
     }
 
-    changeColor(){
-        this.col=color(random(255),random(255),random(255));
+    changeColor() {
+        this.col = color(random(255), random(255), random(255));
     }
 
-    intersects(other){
+    intersects(other) {
         var d = dist(this.x, this.y, other.x, other.y);
         if (d < this.r + other.r) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
