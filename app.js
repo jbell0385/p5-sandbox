d34052c8f3ad7f6d39ce3e32023202b9507bfb1b
Jeremy Bell <jbell0358@gmail.com>
2017-11-07 13:06:26 -0800

    added phobic behavior

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let bubbles = [];
 
 function setup() {
     createCanvas(600, 400);
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 100; i++) {
         bubbles[i] = new Bubble(random(width), random(height));
     }
 }
@@ -13,7 +13,8 @@ function draw() {
         bubbles[i].update();
         bubbles[i].display();
         for (var j = 0; j < bubbles.length; j++) {
-            if (bubbles[i].intersects(bubbles[j]) && bubbles[j]!==bubbles[i]) {
+            if (bubbles[i].intersects(bubbles[j]) && i!==j) {
+
                 bubbles[i].changeColor();
                 bubbles[j].changeColor();
             }
@@ -26,18 +27,48 @@ class Bubble {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.r = 48;
+        this.r = 10;
         this.col = color(255);
-
+        this.intersect = false;
     }
     display() {
         stroke(255);
         fill(this.col, 100);
         ellipse(this.x, this.y, this.r * 2);
     }
-    update() {
-        this.x = this.x + random(-1, 1);
-        this.y = this.y + random(-1, 1);
+    update(other) {
+        if(this.intersects && other !== undefined){
+            if(this.y>other.y){
+                this.y = this.y + random(0,3);
+                other.y = other.y + random(-3,0);
+            }else{
+                other.y = other.y + random(0,3);
+                this.y = this.y + random(-3,0);
+            }
+            if(this.x>other.x){
+                this.x = this.x + random(0,3);
+                other.x = other.x + random(-3,0);
+            }else{
+                other.x = other.x + random(0,3);
+                this.x = this.x + random(-3,0);
+            }
+        }
+
+        if(this.x-this.r<=0){
+            this.x=this.x+random(0,3);
+        }else if(this.x+this.r>=width){
+            this.x=this.x+random(-3,0);
+        }else{
+            this.x = this.x + random(-1, 1);
+        }
+
+        if(this.y-this.r<=0){
+            this.y=this.y+random(0,3);
+        }else if(this.y+this.r>=height){
+            this.y=this.y+random(-3,0);
+        }else{
+            this.y = this.y + random(-1, 1);
+        }
     }
 
     changeColor() {
@@ -47,8 +78,11 @@ class Bubble {
     intersects(other) {
         var d = dist(this.x, this.y, other.x, other.y);
         if (d < this.r + other.r) {
+            this.intersect = true;
+            this.update(other);
             return true;
         } else {
+            this.intersect = false;
             return false;
         }
     }
